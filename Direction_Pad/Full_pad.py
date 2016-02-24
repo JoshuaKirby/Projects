1e06589cdc270f312c8d53305ba80b762ecb337d
jokirby <jokirby@U1455763.mccain.com>
2016-02-24 15:50:44 -0400

    Tried to modify the main to produce a change based on direction without hard coding. didn't work

--- Direction_Pad/Full_pad.py
+++ Direction_Pad/Full_pad.py
@@ -12,6 +12,9 @@ class Window(tk.Frame):
     arrow = {0:"",1:"↑",2:"", \
              3:"←",4:"",5:"→", \
              6:"",7:"↓",8:""}
+    command = {0:onClick(0),1:onClick(1),2:onClick(2), \
+             3:onClick(3),4:onClick(4),5:onClick(5), \
+             6:onClick(6),7:onClick(7),8:onClick(8)}
     
     #image = {"UpLeft":Image.open("Images/UpLeft.png"),"Up":Image.open("Images/Up.png"),"UpRight":Image.open("Images/UpRight.png"), \
     #         "Left":Image.open("Images/Left.png"),"Center":Image.open("Images/Up.png"),"Right":Image.open("Images/Right.png"), \
@@ -32,10 +35,17 @@ class Window(tk.Frame):
         self.button = {0:tk.Button(text="a"),1:tk.Button(text="↑"),2:tk.Button(text="c"), \
                  3:tk.Button(text="←"),4:tk.Button(text="●"),5:tk.Button(text="→"), \
                  6:tk.Button(text="g"),7:tk.Button(text="↓"),8:tk.Button(text="i")}
+        self.command = {0:self.onClick(0),1:self.onClick(1),2:self.onClick(2), \
+             3:self.onClick(3),4:self.onClick(4),5:self.onClick(5), \
+             6:self.onClick(6),7:self.onClick(7),8:self.onClick(8)}
         i = 0
         for item in self.button.values():
                 item.grid(row = int(i/3),column = int(i%3))
+                
+                item["command"] = item.onClick(i)
                 i = i + 1
+    def onClick(self,direction):
+         print(direction)   
         
     
     #===========================================================================
@@ -58,11 +68,9 @@ class Window(tk.Frame):
     #     button["image"] = img
     #     button["command"] = self.onClick(direction)
     #     return button
-    #     
-    # def onClick(self, direction):
-    #     print(direction)
-    #===========================================================================
-   
+    #===========================================     
+    
+       
 root = tk.Tk()
 app = Window(master=root)
 app.mainloop()
